b33f3fa870fc1a25aa46f6b2e80db6fde92d09d8
Gott-ist-tot <mr.kuo0719@gmail.com>
2026-04-16 21:55:33 +0800

    publish: 2026-04-16 21:55

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        body: "Lora",
         code: "IBM Plex Mono",
       },
       colors: {
